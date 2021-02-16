185cef414cb43d0ab4f14efca9d2dc8195fc7da1
safgen <sheirafgen@gmail.com>
2021-02-16 15:42:37 +0900

    added convLSTMS to basicLSTMs.py

--- basicLSTMs.py
+++ basicLSTMs.py
@@ -1,7 +1,9 @@
 from numpy import array
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Bidirectional
+from keras.layers import LSTM, Dense, Bidirectional, Conv1D, TimeDistributed, MaxPooling1D, Flatten, ConvLSTM2D
 
+
+TYPE = "stackedLSTM"  # Type of univariate LSTM - Default = Vanilla LSTM
 
 # split a univariate sequence into samples
 def split_sequence(sequence, n_steps):
@@ -46,21 +48,77 @@ def get_bidirectional_lstm(n_units=50):
     model.compile(optimizer='adam', loss='mse')
     return model
 
+
+def get_cnn_lstm(n_units=50, filters=64):
+    model = Sequential()
+    model.add(
+        TimeDistributed(Conv1D(filters=filters, kernel_size=1, activation='relu'),
+                        input_shape=(None, n_steps, n_features)))
+    model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+    model.add(TimeDistributed(Flatten()))
+    model.add(LSTM(n_units, activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    return model
+
+
+def get_conv_lstm(n_units=50, filters=64):
+    model = Sequential()
+    model.add(
+        ConvLSTM2D(filters=64, kernel_size=(1, 2), activation='relu', input_shape=(n_seq, 1, n_steps, n_features)))
+    model.add(Flatten())
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    return model
+
 # define input sequence
 raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-# choose a number of time steps
-n_steps = 3
-# split into samples
-X, y = split_sequence(raw_seq, n_steps)
-# reshape from [samples, timesteps] into [samples, timesteps, features]
+
 n_features = 1
-X = X.reshape((X.shape[0], X.shape[1], n_features))
+if TYPE not in ["CNNLSTM", "ConvLSTM"]:
+    # choose a number of time steps
+    n_steps = 3
+    # split into samples
+    X, y = split_sequence(raw_seq, n_steps)
+    # reshape from [samples, timesteps] into [samples, timesteps, features]
+    X = X.reshape((X.shape[0], X.shape[1], n_features))
+else:
+    # choose a number of time steps
+    n_steps = 4
+    # split into samples
+    X, y = split_sequence(raw_seq, n_steps)
+    n_steps = 2
+    n_seq = 2
+    if TYPE == "CNNLSTM":
+        X = X.reshape((X.shape[0], n_seq, n_steps, n_features))
+    else:
+        X = X.reshape((X.shape[0], n_seq, 1, n_steps, n_features))
+
 # define model
-model = get_stacked_lstm()
+print('model type is {0}'.format(TYPE))
+if TYPE == "CNNLSTM":
+    model = get_cnn_lstm()
+elif TYPE == "ConvLSTM":
+    model = get_conv_lstm()
+elif TYPE == "BidirectionalLSTM":
+    model = get_bidirectional_lstm()
+elif TYPE == "StackedLSTM":
+    model = get_stacked_lstm()
+else:
+    model = get_vanilla_lstm()
 # fit model
 model.fit(X, y, batch_size=2, epochs=200, verbose=1)
 # demonstrate prediction
 x_input = array([70, 80, 90])
-x_input = x_input.reshape((1, n_steps, n_features))
+if TYPE not in ["CNNLSTM", "ConvLSTM"]:
+    x_input = array([70, 80, 90])
+    x_input = x_input.reshape((1, n_steps, n_features))
+else:
+    x_input = array([60, 70, 80, 90])
+    if TYPE == "CNNLSTM":
+        x_input = x_input.reshape((1, n_seq, n_steps, n_features))
+    else:
+        x_input = x_input.reshape((1, n_seq, 1, n_steps, n_features))
+
 yhat = model.predict(x_input, verbose=0)
 print(yhat)
